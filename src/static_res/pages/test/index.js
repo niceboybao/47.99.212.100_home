a44e0e723a6a661aa35efc17cbcc5658854d120d
baldwin <niceboybao@gmail.com>
2018-12-14 11:32:50 +0800

    ［需求／BUG］: 浏览器指纹 ［模块］: 无 ［说明］: 无

--- src/static_res/pages/test/index.js
+++ src/static_res/pages/test/index.js
@@ -1,1 +1,8 @@
 alert(navigator.userAgent.toLowerCase());
+
+new Fingerprint2().get(function(result, components){
+    alert(result);
+    console.log(result); //a hash, representing your device fingerprint
+    console.log(components); // an array of FP components
+});
+
