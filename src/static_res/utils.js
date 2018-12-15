b4aeab0e2802f11f67931269951d3e111eb13e5e
baldwin <niceboybao@gmail.com>
2018-12-15 18:08:39 +0800

    ［需求／BUG］: 更加url自动开关日志 ［模块］: 无 ［说明］: 无

--- src/static_res/utils.js
+++ src/static_res/utils.js
@@ -2,14 +2,17 @@
  * @Author: guangwei.bao 
  * @Date: 2018-12-15 17:54:35 
  * @Last Modified by: guangwei.bao
- * @Last Modified time: 2018-12-15 17:57:22
+ * @Last Modified time: 2018-12-15 18:07:44
  * @Describe: static_res 工具类
  */
 
-
 (function(util) {
-    window.isDebugger = false; //false为生产模式，true为调试模式
-    // window.isDebugger = true; //false为生产模式，true为调试模式
+	window.isDebugger = true; //false为生产模式，true为调试模式
+	//线上环境
+	if (window.location.href.indexOf('niceboybao.com')>=0) {
+		window.isDebugger = false; //false为生产模式，true为调试模式
+	}
+
 	console.log = (function(oriLogFunc) {
 		return function(str) {
 			if (window.isDebugger) {
